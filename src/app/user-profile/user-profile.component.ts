abb70f3b8910a902ec394c37c47105ce0bae9152
Leo Choi <choimankin@gmail.com>
2017-03-24 00:09:50 +0800

    1. Google maps

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivityDto} from "../../abp-http/ut-api-js-services/model/ActivityDto";
-import {App_activityApi} from "../../abp-http/ut-api-js-services/api/App_activityApi";
-import {FileDto} from "../../abp-http/ut-api-js-services/model/FileDto";
-import {App_fileApi} from "../../abp-http/ut-api-js-services/api/App_fileApi";
+import { ActivityDto } from '../../abp-http/ut-api-js-services/model/ActivityDto';
+import { App_activityApi } from '../../abp-http/ut-api-js-services/api/App_activityApi';
+import { UserDto } from '../../abp-http/ut-api-js-services/model/UserDto';
+import { App_userApi } from '../../abp-http/ut-api-js-services/api/App_userApi';
 
 @Component({
   selector: 'app-user-profile',
@@ -12,8 +12,10 @@ import {App_fileApi} from "../../abp-http/ut-api-js-services/api/App_fileApi";
 export class UserProfileComponent implements OnInit {
 
   public myActivities: ActivityDto[];
+  public myUser: UserDto;
 
-  constructor(private activityService: App_activityApi) {
+  constructor(private activityService: App_activityApi,
+              private userService: App_userApi) {
   }
 
   ngOnInit() {
@@ -23,31 +25,12 @@ export class UserProfileComponent implements OnInit {
         this.myActivities = output.myActivities;
         console.log(this.myActivities);
       });
+
+    this.userService
+      .appUserGetMyUser()
+      .subscribe((output) => {
+        this.myUser = output.myUser;
+      });
   }
 
 }
-
-// function isElementInViewport(el) {
-//   var rect = el.getBoundingClientRect();
-//   return (
-//     rect.top >= 0 &&
-//     rect.left >= 0 &&
-//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//   );
-// }
-//
-// var items = document.querySelectorAll(".timeline li");
-//
-// // code for the isElementInViewport function
-//
-// function callbackFunc() {
-//   for (var i = 0; i < items.length; i++) {
-//     if (isElementInViewport(items[i])) {
-//       items[i].classList.add("in-view");
-//     }
-//   }
-// }
-//
-// window.addEventListener("load", callbackFunc);
-// window.addEventListener("scroll", callbackFunc);
