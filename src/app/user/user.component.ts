9fcfd8ae6fc8f65473c85502aad3c7a5b251fa59
Leo Choi <choimankin@gmail.com>
2017-04-03 16:41:55 +0800

    1. Update UserPage

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,6 +4,7 @@ import { App_activityApi } from '../../abp-http/ut-api-js-services/api/App_activ
 import { UserDto } from '../../abp-http/ut-api-js-services/model/UserDto';
 import { ActivityDto } from '../../abp-http/ut-api-js-services/model/ActivityDto';
 import { ActivatedRoute } from '@angular/router';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Component({
   selector: 'app-user',
@@ -12,60 +13,90 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserComponent implements OnInit {
 
-  public myActivities: ActivityDto[];
-  public myUser: UserDto;
-  id: number;
-  private sub;
+  public isMyUser = false;
+  public activities: ActivityDto[] = [];
+  public user: UserDto;
 
-  private checkMyUser() {
-    const storedMyUser = localStorage.getItem('myUser');
-    let isMyUser: boolean;
-    // console.log(this.id);
-    // console.log(JSON.parse(storedMyUser).id);
-
-    if (this.id === JSON.parse(storedMyUser).id) {
-      isMyUser = true;
-    } else {
-      isMyUser = false;
-    }
-    console.log("isMyUser:" + isMyUser);
-    return isMyUser;
-  }
+  private id: number;
 
   constructor(private route: ActivatedRoute,
+              private localStorageService: LocalStorageService,
               private activityService: App_activityApi,
               private userService: App_userApi) {
   }
 
   ngOnInit() {
-    this.userService
-      .appUserGetMyUser()
-      .subscribe((output) => {
-        this.myUser = output.myUser;
-        localStorage.setItem('myUser', JSON.stringify(this.myUser));
+    this.route.params
+      .map(params => {
+        const id: number = params['id'];
+
+        this.id = id;
+
+        return id;
+      })
+      .subscribe(id => {
+        this.isMyUser = this.checkIsMyUser(id);
+
+        if (this.isMyUser) {
+          this.getMyUserAndMyActivities();
+        } else {
+          this.getUserAndActivities(id);
+        }
       });
-
-    this.sub = this.route.params.subscribe(params => {
-      this.id = +params['id'];
-    });
-
-    // this.checkMyUser();
-
-    if (this.checkMyUser() === true) {
-      this.activityService
-        .appActivityGetMyActivities({})
-        .subscribe((output) => {
-          this.myActivities = output.myActivities;
-          console.log(this.myActivities);
-        });
-    } else {
-      // Get Another Users' Activities
-    }
-
-
   }
 
+  private checkIsMyUser(userId: number): boolean {
+    const myUser = this.localStorageService.get<UserDto>('myUser');
 
+    return myUser.id === userId;
+  }
+
+  private getMyUserAndMyActivities() {
+    const getMyUserSubscription = this.userService
+      .appUserGetMyUser()
+      .subscribe(output => {
+        this.user = output.myUser;
+
+        this.localStorageService.set('myUser', output.myUser);
+        this.localStorageService.set('userGuestId', output.guestId);
+
+        getMyUserSubscription.unsubscribe();
+      });
+
+    const getMyActivitiesSubscription = this.activityService
+      .appActivityGetMyActivities()
+      .subscribe(output => {
+        const activities = output.myActivities;
+
+        for (let i = 0; i < activities.length; i++) {
+          this.activities.push(activities[i]);
+        }
+
+        getMyActivitiesSubscription.unsubscribe();
+      });
+  }
+
+  private getUserAndActivities(id) {
+    const getUserSubscription = this.userService
+      .appUserGetUser({id: id})
+      .subscribe(output => {
+        this.user = output.user;
+
+        getUserSubscription.unsubscribe();
+      });
+
+    const getActivitiesSubscription = this.activityService
+      .appActivityGetActivities({userId: id})
+      .subscribe(output => {
+        const activities = output.activities;
+
+        for (let i = 0; i < activities.length; i++) {
+          this.activities.push(activities[i]);
+        }
+
+        getActivitiesSubscription.unsubscribe();
+      });
+  }
 
 }
 
