36a03aa02eeee70e083f0a7a3ef02bd30de4b6f3
Nelson Lee <ctnelsoncar@gmail.com>
2017-04-12 06:21:17 +0800

    1. did world page - Added image, UniTime and slogan 2. did activity-plan page - comments: icon problem - loop out the activity templates in the activity plan

--- src/app/activity-plan/activity-plan.component.ts
+++ src/app/activity-plan/activity-plan.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivityPlanDto } from '../../abp-http/ut-api-js-services/model/ActivityPlanDto';
-import { CreateTextCommentInput } from '../../abp-http/ut-api-js-services/model/CreateTextCommentInput';
-import { ActivatedRoute } from '@angular/router';
-import { App_activityPlanApi } from '../../abp-http/ut-api-js-services/api/App_activityPlanApi';
-import { App_commentApi } from '../../abp-http/ut-api-js-services/api/App_commentApi';
+import {Component, OnInit} from '@angular/core';
+import {ActivityPlanDto} from '../../abp-http/ut-api-js-services/model/ActivityPlanDto';
+import {CreateTextCommentInput} from '../../abp-http/ut-api-js-services/model/CreateTextCommentInput';
+import {ActivatedRoute} from '@angular/router';
+import {App_activityPlanApi} from '../../abp-http/ut-api-js-services/api/App_activityPlanApi';
+import {App_commentApi} from '../../abp-http/ut-api-js-services/api/App_commentApi';
+import {CreateReplyInput} from '../../abp-http/ut-api-js-services/model/CreateReplyInput';
+import {UserDto} from '../../abp-http/ut-api-js-services/model/UserDto';
+import {App_userApi} from '../../abp-http/ut-api-js-services/api/App_userApi';
+import {LocalStorageService} from 'ng2-webstorage';
+import {App_replyApi} from '../../abp-http/ut-api-js-services/api/App_replyApi';
+import {CommentDto} from '../../abp-http/ut-api-js-services/model/CommentDto';
 
 
 @Component({
@@ -18,10 +24,18 @@ export class ActivityPlanComponent implements OnInit {
   public createTextCommentInput: CreateTextCommentInput = {
     content: ''
   };
+  public createReplyInput: CreateReplyInput = {
+    content: ''
+  };
+
+  public user: UserDto;
 
   constructor(private route: ActivatedRoute,
               private activityPlanApi: App_activityPlanApi,
-              private commentApi: App_commentApi) {
+              private commentApi: App_commentApi,
+              private localStorageService: LocalStorageService,
+              private userApi: App_userApi,
+              private replyApi: App_replyApi) {
   }
 
   ngOnInit() {
@@ -32,15 +46,9 @@ export class ActivityPlanComponent implements OnInit {
       this.createTextCommentInput.activityPlanId = id;
 
       this.getActivityPlan();
-    });
-  }
 
-  public onClickTextComment() {
-    this.commentApi
-      .appCommentCreateTextComment(this.createTextCommentInput)
-      .subscribe(output => {
-        this.getActivityPlan();
-      });
+      // this.getUser();
+    });
   }
 
   private getActivityPlan() {
@@ -50,6 +58,31 @@ export class ActivityPlanComponent implements OnInit {
       .appActivityPlanGetActivityPlan({id: this.activityPlanId})
       .subscribe((output) => {
         this.activityPlan = output.activityPlan;
+        console.log('Got activityPlan: ', this.activityPlan);
+      });
+  }
+
+  public onClickCreateTextComment() {
+    const createTextCommentSubscription = this.commentApi
+      .appCommentCreateTextComment(this.createTextCommentInput)
+      .subscribe(output => {
+        this.getActivityPlan();
+
+        createTextCommentSubscription.unsubscribe();
+      });
+  }
+
+  public onClickCreateReply(comment: CommentDto) {
+    this.createReplyInput.commentId = comment.id;
+
+    const createTextCommentSubscription = this.replyApi
+      .appReplyCreateReply(this.createReplyInput)
+      .subscribe(output => {
+        this.getActivityPlan();
+
+        this.createReplyInput.commentId = null;
+
+        createTextCommentSubscription.unsubscribe();
       });
   }
 
