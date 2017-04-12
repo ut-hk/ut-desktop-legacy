e8e5ab74a5ec1ecffe4146ea950e15562e61e9d3
Arthur <arthuryaucs@gmail.com>
2017-04-12 22:52:06 +0800

    1. Added activity invitation function/n2. modified pending activity invitation page UI

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -1,27 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CreateTextCommentInput } from '../../abp-http/ut-api-js-services/model/CreateTextCommentInput';
-import { App_commentApi } from '../../abp-http/ut-api-js-services/api/App_commentApi';
-import { App_activityApi } from '../../abp-http/ut-api-js-services/api/App_activityApi';
-import { CreateReplyInput } from '../../abp-http/ut-api-js-services/model/CreateReplyInput';
-import { CommentDto } from '../../abp-http/ut-api-js-services/model/CommentDto';
-import { App_replyApi } from '../../abp-http/ut-api-js-services/api/App_replyApi';
-import { ActivityDto } from 'abp-http/ut-api-js-services';
-import { UserService } from '../user.service';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CreateTextCommentInput} from '../../abp-http/ut-api-js-services/model/CreateTextCommentInput';
+import {App_commentApi} from '../../abp-http/ut-api-js-services/api/App_commentApi';
+import {App_activityApi} from '../../abp-http/ut-api-js-services/api/App_activityApi';
+import {CreateReplyInput} from '../../abp-http/ut-api-js-services/model/CreateReplyInput';
+import {CommentDto} from '../../abp-http/ut-api-js-services/model/CommentDto';
+import {App_replyApi} from '../../abp-http/ut-api-js-services/api/App_replyApi';
+import {ActivityDto, CreateActivityInvitationsInput} from 'abp-http/ut-api-js-services';
+import {UserService} from '../user.service';
+import {UserListDto} from '../../abp-http/ut-api-js-services/model/UserListDto';
+import {App_relationshipApi} from '../../abp-http/ut-api-js-services/api/App_relationshipApi';
+import {UserDto} from '../../abp-http/ut-api-js-services/model/UserDto';
+import {LocalStorageService} from 'ng2-webstorage';
+import {App_activityInvitationApi} from '../../abp-http/ut-api-js-services/api/App_activityInvitationApi';
+
+
+interface ParticipantIdInput {
+  user: UserListDto;
+  isSelected: boolean;
+}
 
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
   styleUrls: ['./activity.component.scss']
 })
+
 export class ActivityComponent implements OnInit {
 
   public pageControls = {
     isMyUser: false
   };
 
+
+  public isMyActivity = false;
+  public myUser: UserDto = null;
+  public friends: UserListDto[] = null;
+
+  public participantIdInputs: ParticipantIdInput[] = [];
+
+  public createActivityInvitationInput: CreateActivityInvitationsInput = {
+    content: null,
+    activityId: null,
+    inviteeIds: []
+  };
+
   public activityId;
   public activity: ActivityDto;
+  @ViewChild('inviteFriendModal') public inviteFriendModal;
+
 
   public createTextCommentInput: CreateTextCommentInput = {
     content: ''
@@ -35,6 +62,9 @@ export class ActivityComponent implements OnInit {
               private commentApi: App_commentApi,
               private replyApi: App_replyApi,
               private userService: UserService,
+              private relationshipApi: App_relationshipApi,
+              private activityInvitationApi: App_activityInvitationApi,
+              private localStorageService: LocalStorageService,
               private router: Router) {
   }
 
@@ -45,8 +75,10 @@ export class ActivityComponent implements OnInit {
 
         this.activityId = id;
         this.createTextCommentInput.activityId = id;
+        this.createActivityInvitationInput.activityId = id;
 
         this.getActivity();
+
       });
   }
 
@@ -85,6 +117,26 @@ export class ActivityComponent implements OnInit {
       });
   }
 
+  public onClickInviteFriendToActivity() {
+
+    for (let i = 0; i < this.participantIdInputs.length; i++) {
+      if (this.participantIdInputs[i].isSelected == true) {
+        this.createActivityInvitationInput.inviteeIds.push(this.participantIdInputs[i].user.id);
+      } else {
+        if (this.participantIdInputs[i].isSelected == false) {
+
+        }
+      }
+    }
+    this.activityInvitationApi
+      .appActivityInvitationCreateActivityInvitations(this.createActivityInvitationInput)
+      .subscribe((output) => {
+        console.log(output);
+      });
+
+    this.inviteFriendModal.hide();
+  }
+
   private getActivity() {
     this.createTextCommentInput.content = '';
 
@@ -95,6 +147,26 @@ export class ActivityComponent implements OnInit {
 
         this.activity = activity;
         this.pageControls.isMyUser = this.userService.checkIsMyUser(activity.owner.id);
+        if (this.pageControls.isMyUser == true) {
+          this.myUser = this.localStorageService.retrieve('myUser');
+          this.getMyFriends();
+        }
+      });
+  }
+
+  private getMyFriends() {
+    const getFriendsSubscription = this.relationshipApi
+      .appRelationshipGetFriends({
+        targetUserId: this.myUser.id
+      })
+      .subscribe((output) => {
+        this.friends = output.friends;
+        for (let i = 0; i < this.friends.length; i++) {
+          const friendInput: ParticipantIdInput = {user: this.friends[i], isSelected: false};
+          this.participantIdInputs.push(friendInput);
+        }
+
+        getFriendsSubscription.unsubscribe();
       });
   }
 
